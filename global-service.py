4046c7330037bb8aab26ff9e46312ae0256f0819
Carlos Zilves <carloszilves@gmail.com>
2020-10-11 19:07:12 -0300

    Global and Host Service: - Adjusts in process management in the service - Change log method

--- global-service.py
+++ global-service.py
@@ -2,24 +2,34 @@
 
 import logging
 from utils import process
-from multiprocessing import Process, Manager
+import multiprocessing as mp
 
 if __name__ == '__main__':
-	logging.basicConfig(filename='./log/global-service.log', filemode='w', format='%(asctime)s %(levelname)s:%(message)s',
-						datefmt='%d/%m/%Y %H:%M:%S',level=logging.INFO)
+	#logging.basicConfig(filename='./log/global-service.log', filemode='w', format='%(asctime)s %(levelname)s:%(message)s',
+	#					datefmt='%d/%m/%Y %H:%M:%S',level=logging.INFO)
 
-	with Manager() as manager:
+	logGS = logging.getLogger(__name__)
+	logGS.setLevel(logging.INFO)
+	format = logging.Formatter(fmt='%(asctime)s %(levelname)s %(message)s', datefmt='%d/%m/%Y %H:%M:%S')
+	file_handler = logging.FileHandler(filename = './log/global-service.log', mode = 'a')
+	file_handler.setFormatter(format)
+	file_handler.setLevel(logging.INFO)
+	logGS.addHandler(file_handler)
+
+	ctx = mp.get_context('spawn')
+
+	with ctx.Manager() as manager:
 		# Creating Host and Request Lists
 		host_list = manager.list()
 		request_list = manager.list()
 
 		# Creating Global Monitor Process
-		logging.info('Starting Monitor Manager Process')
-		proc1 = Process(target=process.global_monitor, args=(host_list, request_list))
+		logGS.info('Starting Monitor Manager Process')
+		proc1 = ctx.Process(target=process.global_monitor, args=(host_list, request_list))
 
 		# Creating Global Scheduler Process
-		logging.info('Starting Request Scheduler Process')
-		proc2 = Process(target=process.global_scheduler, args=(host_list, request_list))
+		logGS.info('Starting Request Scheduler Process')
+		proc2 = ctx.Process(target=process.global_scheduler, args=(host_list, request_list))
 
 		# Starting Created Processes
 		proc1.start()
--- host-service.py
+++ host-service.py
@@ -1,32 +1,45 @@
 #!/usr/bin/env python3
-
 import logging
 from utils import process
-from classes.host import Host
-from multiprocessing import Process, Manager
+import multiprocessing as mp
 
 if __name__ == '__main__':
-	logging.basicConfig(filename='./log/host-service.log', filemode='w', format='%(asctime)s %(levelname)s:%(message)s',
-						datefmt='%d/%m/%Y %H:%M:%S', level=logging.INFO)
+	#logging.basicConfig(filename='./log/host-service2.log', filemode='a', format='%(asctime)s %(levelname)s:%(message)s',
+	#					datefmt='%d/%m/%Y %H:%M:%S', level=logging.INFO)
+	logHS = logging.getLogger('Host_Service')
+	logHS.setLevel(logging.INFO)
+	format = logging.Formatter(fmt='%(asctime)s %(levelname)s %(message)s', datefmt='%d/%m/%Y %H:%M:%S')
+	file_handler = logging.FileHandler(filename = './log/host-service2.log', mode = 'a')
+	file_handler.setFormatter(format)
+	file_handler.setLevel(logging.INFO)
+	logHS.addHandler(file_handler)
 
-	with Manager() as manager:
-		host_queue = manager.Queue()
+	#mp.set_start_method('spawn')
+	ctx = mp.get_context('spawn')
+
+	with ctx.Manager() as manager:
+		shared_list = manager.list()
+		ac_list = []
+		ic_list = []
+		core_list = []
+		shared_list.append(ac_list)
+		shared_list.append(ic_list)
+		shared_list.append(core_list)
+		#print(shared_list)
 		entry_queue = manager.Queue()
-		host = Host()
-		host_queue.put(host)
-		entry_queue.put([])
 
 		# Creating Monitor Process
-		logging.info('Starting Monitor Process')
-		proc1 = Process(target=process.host_monitor, args=(host_queue,))
+		logHS.info('Starting Monitor Process')
+		proc1 = ctx.Process(target=process.host_monitor, args=(shared_list,))
 
 		# Creating Request Receiver Process
-		logging.info('Starting Request Receiver Process')
-		proc2 = Process(target=process.request_receiver2, args=(entry_queue,))
+		logHS.info('Starting Request Receiver Process')
+		proc2 = ctx.Process(target=process.request_receiver, args=(entry_queue,))
 
 		# Creating Container Manager Process
-		logging.info('Starting Container Manager Process')
-		proc3 = Process(target=process.container_manager, args=(host_queue, entry_queue))
+		logHS.info('Starting Container Manager Process')
+		proc3 = ctx.Process(target=process.no_manager, args=(shared_list, entry_queue))
+		#proc3 = ctx.Process(target=process.container_manager2, args=(shared_list, entry_queue))
 
 		# Starting Created Processes
 		proc1.start()
